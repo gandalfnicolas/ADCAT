e413e8c7d7fb94ff751667b87d4cc2eb8a36e83b
gandalfnicolas <gandalfn@gmail.com>
2019-05-14 17:24:55 -0400

    creating files

---
+++ R/Lemmatize.R
@@ -0,0 +1,19 @@
+#' @title Lemmatizer
+#'
+#' @description This function allows you lemmatize words using the treetag lemmatizer
+#' @param word word to lemmatize, if multiple use loop/apply/dplyr
+#' @param print Whether to print word being lemmatized. Useful for long lists of words. Defaults to TRUE
+#' @return lemmatized words
+#' @export Lemmatize
+
+
+Lemmatize = function(word, print =T){
+  if (print == T){
+    print(word)
+  }
+  lemmax = koRpus::treetag(as.character(word), treetagger="manual", format="obj", TT.tknz=T , lang="en", TT.options=list(path="C:\\treetagger", preset="en"))
+  if(lemmax@TT.res[["lemma"]] == "<unknown>"){
+    return (lemmax@TT.res[["token"]])}
+  else
+    return(lemmax@TT.res[["lemma"]])
+}
---
+++ R/data.R
@@ -0,0 +1,70 @@
+#' Unprocessed Full dictionary codings
+#'
+#' Unprocessed Full dictionary codings, including the vector codings. May be used for different degrees of preprocessing. (e.g., use if text too extensive to preprocess)
+#'
+#'
+#' @format A data frame with 14449 rows and 981 variables:
+#' \describe{
+#'   \item{values}{Words as obtained from the literature or Wordnet. No preprocessing}
+#'   \item{values0}{lower-case word values}
+#'   \item{values1}{lower-case word values, no spaces or symbols}
+#'   \item{values2}{lower-case word values, no spaces or symbols, lemmatized}
+#'   \item{values3}{lower-case word values, no spaces or symbols, lemmatized, with no ending Ss (not real words. These are the values averaged over in the final dictionaries)}
+#'   \item{_dict}{variables ending in _dict indicate if the word is (1) or not (0) in the dictionary. If accompanied by a _lo it is coding if the word is low & in the dictionary, and if accompanied by a _hi it is coding if the word is high & in the dictionary (i.e., it combines the _dict and _dir variables)}
+#'   \item{_dir}{variables ending in _dir indicate if the word is high (1), neutral (0) or low (-1) in the dictionary; e.g., friendly is high for sociability; unfriendly is low. Coded as NA if word not in the corresponding dictionary}
+#'   \item{fasttext}{variables starting in fasttext are the word embedding dimensions for Fasttext trained on 2 million word vectors trained with subword information on Common Crawl (https://fasttext.cc/docs/en/english-vectors.html)}
+#'   \item{Glove}{variables starting in Glove are the word embedding dimensions for Glove trained on Common Crawl (840B tokens, 2.2M vocab, cased, 300d vectors; https://nlp.stanford.edu/projects/glove/) (https://fasttext.cc/docs/en/english-vectors.html)}
+#'   \item{Word2vec}{variables starting in Word2vec are the word embedding dimensions for Word2vec trained Google News (https://code.google.com/archive/p/word2vec/)}
+#'   ...
+#' }
+"Unprocessed_Dictionaries"
+
+
+#' Full dictionary codings
+#'
+#' Full dictionary codings, including the vector codings
+#'
+#'
+#' @format A data frame with 13930 rows and 973 variables:
+#' \describe{
+#'   \item{word}{a word in one or more of the dictionaries}
+#'   \item{_dict}{variables ending in _dict indicate if the word is (1) or not (0) in the dictionary. If accompanied by a _lo it is coding if the word is low & in the dictionary, and if accompanied by a _hi it is coding if the word is high & in the dictionary (i.e., it combines the _dict and _dir variables)}
+#'   \item{_dir}{variables ending in _dir indicate if the word is high (1), neutral (0) or low (-1) in the dictionary; e.g., friendly is high for sociability; unfriendly is low. Coded as NA if word not in the corresponding dictionary}
+#'   \item{fasttext}{variables starting in fasttext are the word embedding dimensions for Fasttext trained on 2 million word vectors trained with subword information on Common Crawl (https://fasttext.cc/docs/en/english-vectors.html)}
+#'   \item{Glove}{variables starting in Glove are the word embedding dimensions for Glove trained on Common Crawl (840B tokens, 2.2M vocab, cased, 300d vectors; https://nlp.stanford.edu/projects/glove/) (https://fasttext.cc/docs/en/english-vectors.html)}
+#'   \item{Word2vec}{variables starting in Word2vec are the word embedding dimensions for Word2vec trained Google News (https://code.google.com/archive/p/word2vec/)}
+#'   ...
+#' }
+"Dictionaries"
+
+
+#' Word embeddings for expanded dictionaries
+#'
+#' Word embeddings for expanded dictionaries
+#'
+#'
+#' @format A data frame with 56 rows and 901 variables:
+#' \describe{
+#'   \item{Dictionary}{Name of the dictionary that the word embeddings are representing.}
+#'   \item{fasttext}{variables starting in fasttext are the word embedding dimensions for Fasttext trained on 2 million word vectors trained with subword information on Common Crawl (https://fasttext.cc/docs/en/english-vectors.html)}
+#'   \item{Glove}{variables starting in Glove are the word embedding dimensions for Glove trained on Common Crawl (840B tokens, 2.2M vocab, cased, 300d vectors; https://nlp.stanford.edu/projects/glove/) (https://fasttext.cc/docs/en/english-vectors.html)}
+#'   \item{Word2vec}{variables starting in Word2vec are the word embedding dimensions for Word2vec trained Google News (https://code.google.com/archive/p/word2vec/)}
+#'   ...
+#' }
+"Full_Vectors"
+
+
+#' Word embeddings for seed dictionaries
+#'
+#' Word embeddings for seed dictionaries
+#'
+#'
+#' @format A data frame with 30 rows and 901 variables:
+#' \describe{
+#'   \item{Dictionary}{Name of the dictionary that the word embeddings are representing.}
+#'   \item{fasttext}{variables starting in fasttext are the word embedding dimensions for Fasttext trained on 2 million word vectors trained with subword information on Common Crawl (https://fasttext.cc/docs/en/english-vectors.html)}
+#'   \item{Glove}{variables starting in Glove are the word embedding dimensions for Glove trained on Common Crawl (840B tokens, 2.2M vocab, cased, 300d vectors; https://nlp.stanford.edu/projects/glove/) (https://fasttext.cc/docs/en/english-vectors.html)}
+#'   \item{W2v}{variables starting in W2v are the word embedding dimensions for Word2vec trained Google News (https://code.google.com/archive/p/word2vec/)}
+#'   ...
+#' }
+"Seed_Vectors"
