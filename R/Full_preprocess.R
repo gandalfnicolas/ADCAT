9aaff26947bd064346d582f3ff937fcf76543d11
gandalfnicolas <gandalfn@gmail.com>
2019-06-10 18:16:38 -0400

    changes

--- R/Full_preprocess.R
+++ R/Full_preprocess.R
@@ -12,30 +12,11 @@ Full_preprocess = function(words, parallelize = T, print =T, debug=F){
   res = sapply(res,trimws) #removes whitespace
   res = sapply(res,tolower)
   res = sapply(res,clean_symbols)
-  Lemmatizex = function(word, print = print, debug=debug){
-    if (print == T){
-      print(word)
-    }
-    lemmax = koRpus::treetag(as.character(word), treetagger="manual", format="obj", TT.tknz=T, debug=debug, lang="en", TT.options=list(path="C:\\treetagger", preset="en"))
-    if(lemmax@TT.res[["lemma"]] == "<unknown>"){
-      if (print == T){
-        print(lemmax@TT.res[["token"]])}
-      return (lemmax@TT.res[["token"]])}
-    else{
-      if (print == T){
-        print(lemmax@TT.res[["lemma"]])}
-      return(lemmax@TT.res[["lemma"]])
-    }}
-  if(parallelize == T){
-    cl <- parallel::makeCluster(parallel::detectCores() - 1)
-    parallel::clusterEvalQ(cl, {
-      library(dplyr)
-      library(koRpus.lang.en)})
-    parallel::clusterExport(cl=cl, varlist=c("Lemmatize", "res"), envir = environment())
-    res = parallel::parSapply(cl,res, Lemmatizex)
-    parallel::stopCluster(cl)}
+  if(debug == T){
+    res = sapply(res, Lemmatize_debug)
+  }
   else{
-    res = sapply(res,Lemmatizex)
+    res = sapply(res, Lemmatize)
   }
   res = sapply(res,delete_ending_Ss)
   res = mapply(Spellcheck,raw = words, cleaned = res, MoreArgs = list(rawlist = words, dict_cleaned= Dictionaries$word))
--- R/Lemmatize_debug.R
+++ R/Lemmatize_debug.R
@@ -1,4 +1,4 @@
-#' @title Lemmatizer
+#' @title Lemmatizer with debug
 #'
 #' @description This function allows you lemmatize words using the treetag lemmatizer
 #' @param word word to lemmatize, if multiple use loop/apply/dplyr
@@ -7,17 +7,17 @@
 #' @export Lemmatize
 
 
-Lemmatize = function(word, print =T){
+Lemmatize_debug = function(word, print =T){
   if (print == T){
     print(word)
   }
-  lemmax = koRpus::treetag(as.character(word), treetagger="manual", format="obj", TT.tknz=T, lang="en", TT.options=list(path="C:\\treetagger", preset="en"))
+  lemmax = koRpus::treetag(as.character(word), treetagger="manual", format="obj", TT.tknz=T, debug=T, lang="en", TT.options=list(path="C:\\treetagger", preset="en"))
   if(lemmax@TT.res[["lemma"]] == "<unknown>"){
     if (print == T){
-    print(lemmax@TT.res[["token"]])}
+      print(lemmax@TT.res[["token"]])}
     return (lemmax@TT.res[["token"]])}
   else{
     if (print == T){
-    print(lemmax@TT.res[["lemma"]])}
+      print(lemmax@TT.res[["lemma"]])}
     return(lemmax@TT.res[["lemma"]])
-}}
+  }}
