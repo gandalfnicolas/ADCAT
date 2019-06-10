f47d769a176aa59a5bc980a59ebc2f68a61f2748
gandalfnicolas <gandalfn@gmail.com>
2019-06-10 18:59:18 -0400

    Update Full_preprocess.R

--- R/Full_preprocess.R
+++ R/Full_preprocess.R
@@ -12,6 +12,17 @@ Full_preprocess = function(words, parallelize = T, print =T, debug=F){
   res = sapply(res,trimws) #removes whitespace
   res = sapply(res,tolower)
   res = sapply(res,clean_symbols)
+  if(parallelize == T){
+    cl <- parallel::makeCluster(parallel::detectCores() - 1)
+    parallel::clusterEvalQ(cl, {
+      library(dplyr)
+      library(koRpus.lang.en)})
+    parallel::clusterExport(cl=cl, varlist=c("Lemmatize", "res"), envir = environment())
+    res = parallel::parSapply(cl,res, Lemmatize)
+    parallel::stopCluster(cl)}
+  else{
+    res = sapply(res,Lemmatize)
+  }
   res = sapply(res, Lemmatize, debug = debug)
   res = sapply(res,delete_ending_Ss)
   res = mapply(Spellcheck,raw = words, cleaned = res, MoreArgs = list(rawlist = words, dict_cleaned= Dictionaries$word))
